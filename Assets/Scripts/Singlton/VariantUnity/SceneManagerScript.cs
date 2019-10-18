066b0277b212cdd3c8f56bef8a3c84f3233d5f6e
vlad <dizel-man@mail.ru>
2019-10-18 18:07:20 +0300

    singlton

---
+++ Assets/Scripts/Singlton/VariantUnity/SceneManagerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+//тут мы просто смотрим пример
+
+public class SceneManagerScript : MonoBehaviour
+{
+    // для удобства
+    public Text ValueTxt;
+
+    void Start()
+    {
+        //это мы просто смотрим в UI сколько раз мы будем переходить
+        ValueTxt.text = SingltonManager.Instance.Value.ToString();
+    }
+
+    // надо для примера иметь две сцены
+    //идем в первую сцену и смотрим сколько раз мы это делали без изменения количеств
+    public void GoToFirstScene()
+    {
+        SceneManager.LoadScene(0);
+        SingltonManager.Instance.Value++;
+
+        //и ещё добавил один объект в сцену
+        SingltonManagerTwo.Instance.Value++;
+    }
+
+    //идем во вторую сцену и смотрим сколько раз
+    public void GoToSecondScene()
+    {
+        SceneManager.LoadScene(1);
+        SingltonManager.Instance.Value++;
+
+        SingltonManagerTwo.Instance.Value++;
+    }
+}
---
+++ Assets/Scripts/Singlton/VariantUnity/SingltonManagerTwo.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+//это пример для сохранения такого скрипта в каждой сцене в единственном числе
+//это синглтон
+//надо его бросить на объект который хотим сохранять в единственном числе в любой сцене
+public class SingltonManagerTwo : MonoBehaviour
+{
+    //только get 
+    //private set - чтобы никто не мог его стереть
+    public static SingltonManagerTwo Instance { get; private set; }
+
+    //делается для подсчета только gameobjectы - не для этого класса
+    public int Value;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            // в сцене появится вкладка DontDestroyOnLoad и под ней будет наш 
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
