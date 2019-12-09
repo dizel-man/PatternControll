bc8fa808f00319be795940a6f09b80adc367108f
dizel-man <vladislav_kriukov@epam.com>
2019-12-09 22:41:41 +0300

    dependency injection

---
+++ Assets/Scripts/Dependency Injection/DependencyLogic.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DependencyLogic
+{
+    private Iinterface _inter;
+
+    public DependencyLogic(Iinterface inter)
+    {
+        this._inter = inter;
+        _inter.Execute();
+
+    }
+}
---
+++ Assets/Scripts/Dependency Injection/RunDependency.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunDependency : MonoBehaviour
+{
+    DependencyLogic logicOne;
+    DependencyLogic logicTwo;
+
+    void Start()
+    {
+        logicOne = new DependencyLogic(new ClassOne());
+        Debug.Log("--");
+        logicTwo = new DependencyLogic(new ClassTwo());
+    }
+
+   
+}
