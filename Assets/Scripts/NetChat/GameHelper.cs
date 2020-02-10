818ce57261fbe903c5298e6cab410538d393dfae
vlad <dizel-man@mail.ru>
2020-02-10 16:18:06 +0300

    net chat

---
+++ Assets/Scripts/NetChat/GameHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//скрипт который отвечает за весь процесс
+
+
+public class GameHelper : MonoBehaviour
+{
+
+    public InputField inputField;
+    public Text textBlock;
+
+    // GameHelper должен знать с каким игроком он
+    // сейчас имеет дело поэтому
+   private PlayerHelper _currentplayerHelper;
+
+    public PlayerHelper CurrentPlayerHelper
+    {
+        get { return _currentplayerHelper; }
+        set { _currentplayerHelper = value; }
+    }
+
+   
+
+    public void Send()
+    {
+        CurrentPlayerHelper.CmdSend(inputField.text);
+    }
+}
---
+++ Assets/Scripts/NetChat/PlayerHelper.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerHelper : NetworkBehaviour
+{
+
+    GameHelper _gameHelper;
+
+    void Start()
+    {
+        _gameHelper = GameObject.FindObjectOfType<GameHelper>();
+        if (isLocalPlayer)
+            _gameHelper.CurrentPlayerHelper = this;
+    }
+
+
+   
+
+    //[Command] значит этот метод вызывается только на сервере
+    [Command]
+    public void CmdSend( string msg)
+    {
+        Debug.Log("[command] Send " + msg);
+        RpcSend(msg);
+    }
+
+
+    // [ClientRpc] значит этот метод высылается всем клиентам
+    [ClientRpc]
+    public void RpcSend( string msg)
+    {
+        _gameHelper.textBlock.text += System.Environment.NewLine + msg;
+    }
+
+}
